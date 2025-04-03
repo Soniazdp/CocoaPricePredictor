313a7edeb6d925364322088cc2a2b9b3b3883a5c
deyikong <kongdeyi0209@gmail.com>
2025-04-03 12:20:39 -0400

    Update 00-Univariate.R

--- scripts/00-Univariate.R
+++ scripts/00-Univariate.R
@@ -1,80 +1,52 @@
 #### Preamble ####
 # Purpose: Fit some univariate model to forecast using Daily Prices_ICCO
-# Author: Deyi Kong
-# Date: March 31th, 2025
-# Contact: deyi.kong@mail.utoronto.ca
-# License: MIT
-# Pre-requisites: The `dplyr`, `lubridate`,`forecast`, `tseries`, and `zoo` packages must be installed
+# Date: Apr 2, 2025
+# Pre-requisites: The `dplyr`, `forecast`, `tseries`, `rugarch`, `fGarch`, and `xts` packages must be installed
 # Any other information needed? Make sure you are in the `CocoaPricePredictor` rproj
 
-install.packages("rugarch")
-library(rugarch)
+
 library(dplyr)
-library(lubridate)
-library(zoo)
 library(forecast)
 library(tseries) 
 library(ggplot2)
-install.packages("fGarch")
+library(Metrics)
+library(rugarch)
 library(fGarch)
 library(xts)
 
 
-# Load dataset
-daily_price <- read.csv("Daily Prices_ICCO (1).csv")
-
-#### Data cleaning ####
-# Convert to monthly data
-monthly_price <- daily_price %>%
-  mutate(price = as.numeric(gsub(",", "", ICCO.daily.price..US..tonne.)), date = as.Date(Date, format = "%d/%m/%Y")) %>% 
-  mutate(month = as.yearmon(date)) %>% 
-  group_by(month) %>%
-  summarise(
-    # Monthly average
-    price = mean(price, na.rm = TRUE)
-  ) %>%
-  arrange(month)  # Sort chronologically
-
-# Check missing data
-is.na(monthly_price)
-
-### Save monthly data ###
-write.csv(monthly_price, "data/Monthly_Prices_ICCO.csv")
-
-
-#### Split training and testing sets ####
-# Consist with later multivariate data spliting
-split_month <- as.yearmon("Feb 2020")
-
-# Split into train/test
-uni_train <- monthly_price %>% filter(month < split_month)
-uni_test <- monthly_price %>% filter(month >= split_month)
+# Load datasets
+train <- read.csv("data/train_simple.csv")
+test <- read.csv("data/test_simple.csv")
 
 # Convert time series
-uni_train_ts <- ts(uni_train$price, start = c(1994, 10), frequency = 12)
-uni_test_ts <- ts(uni_test$price, start = c(2020, 2), frequency = 12)
-
+uni_train_ts <- ts(train$price_usd_per_tonne_mean, start = c(1996, 1), frequency = 12)
+uni_test_ts <- ts(test$price_usd_per_tonne_mean, start = c(2019, 2), frequency = 12)
+  
 
 #### Fit ETS model ####
-ets_model <- ets(uni_train_ts, model ="ZZZ")
-ets_model # AIC: 4599.675
+ets_model <- ets(uni_train_ts, model ="ZZZ") # ETS(M,N,N)
+ets_model # AIC: 4186.622
 
 # Forecast on test set
 forecast_ets <- forecast(ets_model, h = length(uni_test_ts))
-accuracy(forecast_ets, uni_test_ts)  # test RMSE(2676.9524) 25× higher than training RMSE(117.0653); ACF1(0.9087675) residual strong pattern 
+accuracy(forecast_ets, uni_test_ts)  # test RMSE(2071.7702) 17× higher than training RMSE(120.2793); ACF1(0.9072913) residual strong pattern 
+
+#the coeffitient of the x_t-1 something like that
 
 # Plot forecast vs actual for ETS
 autoplot(forecast_ets) +
-  autolayer(uni_test_ts, series = "Test Data", PI = FALSE) +
+  autolayer(uni_test_ts, series = "Test Data") +
   ggtitle("ETS Forecast vs Test Data") +
   xlab("Year") + ylab("Price (USD/tonne)") +
   theme_minimal()
+
 
 #### Fit SARIMA model ####
 sarima_model <- auto.arima(uni_train_ts, seasonal = TRUE) # auto-tuning ARIMA(0,1,1) -> no strong seasonality detected
 summary(sarima_model) # AIC: 3734.32
 
-###graph of residuals, Ljung-Box test, Histogram and time plot
+# graph of residuals, Ljung-Box test, Histogram and time plot
 checkresiduals(sarima_model)
 
 # Forecast on test set
@@ -84,12 +56,14 @@ accuracy(forecast_sarima, uni_test_ts)  # test RMSE(2657.6344) 23× higher than 
 # Plot forecast vs actual for SARIMA
 autoplot(forecast_sarima) +
   autolayer(uni_test_ts, series = "Test Data", PI = FALSE) +
-  ggtitle("SARIMA Forecast vs Test Data") +
+  ggtitle("SARIMA Forecast vs. Test Data") +
   xlab("Year") + ylab("Price (USD/tonne)") +
   theme_minimal()
 
-#pick the price column in the monthly price df
-price_series <- monthly_price$price
+
+#### Fit GARCH Model ####
+# pick the price column in the monthly price df
+price_series <- train$price_usd_per_tonne_mean
 
 # Compute log returns
 cocoa_returns <- diff(log(price_series))[-1]
@@ -102,3 +76,6 @@ garch_cocoa <- garchFit(~ arma(1, 0) + garch(1, 1), data = cocoa_returns,
                         cond.dist = "std", trace = FALSE)
 summary(garch_cocoa)
 predict(garch_cocoa, n.ahead = 12, plot = TRUE)  # Forecast 12 periods ahead
+
+
+
